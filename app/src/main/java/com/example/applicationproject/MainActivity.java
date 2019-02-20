1c6b14cd47b11a2ca2518123d5a2352efbe21b94
Garrett Smith <smi16166@byui.edu>
2019-02-20 09:44:35 -0700

    Comment change Garrett

--- app/src/main/java/com/example/applicationproject/MainActivity.java
+++ app/src/main/java/com/example/applicationproject/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.applicationproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-//Let's make accounts and calendar first
+//Let's make accounts and calendar first - Garrett
 public class MainActivity extends AppCompatActivity {
 
     @Override
