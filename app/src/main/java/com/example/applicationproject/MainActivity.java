c0638908487beb65674bcceb36f655832cfd2fa8
czzzz97 <cshughes97@yahoo.com>
2019-02-20 09:44:34 -0700

    Alter commit

--- app/src/main/java/com/example/applicationproject/MainActivity.java
+++ app/src/main/java/com/example/applicationproject/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.applicationproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-//Let's make accounts and calendar first
+//Let's make some accounts but calendar first - Corbin
 public class MainActivity extends AppCompatActivity {
 
     @Override
