c5c833b1adc6e6f538cc2e39a0aa9b678a997747
Garrett Smith <smi16166@byui.edu>
2019-02-27 10:04:30 -0700

    Added Sample Test class as example

---
+++ app/src/test/java/com/example/applicationproject/TestSample.java
@@ -0,0 +1,13 @@
+package com.example.applicationproject;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestSample {
+
+    @Test
+    public void testAddition() {
+        assertEquals(4, 2+2);
+    }
+}
