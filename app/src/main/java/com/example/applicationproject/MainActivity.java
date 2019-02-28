02c71bcde3770bf235c2cfbc799ae98bac3a6190
Richard Hawkins <47014116+rhawkins32@users.noreply.github.com>
2019-02-27 19:57:01 -0700

    Added first implementation of MainActivity

--- app/src/main/java/com/example/applicationproject/MainActivity.java
+++ app/src/main/java/com/example/applicationproject/MainActivity.java
@@ -1,15 +1,35 @@
 package com.example.applicationproject;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 //Let's make accounts but calendar first  - Garrett/Corbin
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        mAuth = FirebaseAuth.getInstance();
+    }
+    
     // this is richard trying to figure out github on my desktop
 }
