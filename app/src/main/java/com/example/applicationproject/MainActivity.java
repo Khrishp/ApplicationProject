913a7692b0931dae32111fc694cda9b873cb84f3
czzzz97 <cshughes97@yahoo.com>
2019-03-09 21:12:05 -0700

    Test out pushing data to the database.

--- app/src/main/java/com/example/applicationproject/MainActivity.java
+++ app/src/main/java/com/example/applicationproject/MainActivity.java
@@ -6,16 +6,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 public class MainActivity extends AppCompatActivity {
 
     FirebaseAuth mAuth;
+    TextView myText;
 
     @Override
     public void onStart(){
@@ -31,6 +35,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mAuth = FirebaseAuth.getInstance();
+    }
+
+    public void createNews(View view) {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+        DatabaseReference myRef2 = database.getReference("test2");
+
+        myRef2.setValue("A more stable test");
+        myText = findViewById(R.id.TEMPORARY);
+        myRef.setValue(myText);
     }
 
     public void goToCalendar(View view) {
