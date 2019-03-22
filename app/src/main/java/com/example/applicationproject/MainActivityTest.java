730e874631426cb0162ac9a88723179914068ecd
Garrett Smith <smi16166@byui.edu>
2019-03-22 02:59:39 -0600

    Created new test xml files with what I propose the app to look like as well as created 95 percent of the adapter and recyler view that we can use for the news and user list. The code works and I didn't want to change all of the code to work with the xml files until I get a go ahead from you guys. The list view currently looks not super cool but it will look ok after the code is all implemented.

---
+++ app/src/main/java/com/example/applicationproject/MainActivityTest.java
@@ -0,0 +1,16 @@
+package com.example.applicationproject;
+
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class MainActivityTest extends AppCompatActivity {
+
+    private static final String TAG = "MainActivityTest";
+
+    //vars
+    private ArrayList<String>  mNames = new ArrayList<>();
+    private ArrayList<String>  mImgeURLS = new ArrayList<>();
+
+    
+}
---
+++ app/src/main/java/com/example/applicationproject/RecyclerViewAdapter.java
@@ -0,0 +1,72 @@
+package com.example.applicationproject;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>{
+
+    private static final String TAG = "RecyclerViewAdapter";
+
+    private ArrayList<String> mImageNames = new ArrayList<>();
+    private ArrayList<String> mImages = new ArrayList<>();
+    private Context mContext;
+
+    public RecyclerViewAdapter(ArrayList<String> mImageNames, ArrayList<String> mImages, Context mContext) {
+        this.mImageNames = mImageNames;
+        this.mImages = mImages;
+        this.mContext = mContext;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.news_listitem, parent, false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, final int i) {
+        Log.d(TAG, "onBindViewHolder: called.");
+
+        viewHolder.imageText.setText(mImageNames.get(i));
+        viewHolder.parentLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: clicked on: " + mImageNames.get(i));
+
+                Toast.makeText(mContext, mImageNames.get(i), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mImageNames.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView placeholder;
+        TextView imageText;
+        RelativeLayout parentLayout;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            placeholder = itemView.findViewById(R.id.placeHolder);
+            imageText = itemView.findViewById(R.id.dummyText);
+            parentLayout = itemView.findViewById(R.id.parent_layout);
+        }
+    }
+}
