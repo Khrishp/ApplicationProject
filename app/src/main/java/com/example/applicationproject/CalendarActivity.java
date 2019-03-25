c9c3b778dcaa267674b0f775cd749843c13b2705
rhawkins32 <haw16019@byui.edu>
2019-03-25 13:32:36 -0600

    Fixing a few things on the loginActvity. Got rid of the calendar ui and we will be starting from scratch on it

--- app/src/main/java/com/example/applicationproject/CalendarActivity.java
+++ app/src/main/java/com/example/applicationproject/CalendarActivity.java
@@ -1,76 +1,12 @@
 package com.example.applicationproject;
 
-import android.content.Intent;
-import android.graphics.RectF;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-
-import com.alamkanak.weekview.MonthLoader;
-import com.alamkanak.weekview.WeekView;
-import com.alamkanak.weekview.WeekViewEvent;
-
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
+import android.util.Log;
 
 
-public class CalendarActivity extends AppCompatActivity implements WeekView.EventClickListener,
-        MonthLoader.MonthChangeListener, WeekView.EventLongPressListener {
-    WeekView mWeekView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        MonthLoader.MonthChangeListener mMonthChangeListener = new MonthLoader.MonthChangeListener() {
-            @Override
-            public List<WeekViewEvent> onMonthChange(int newYear, int newMonth) {
-                // Populate the week view with some events.
-                List<WeekViewEvent> events = getEvents(newYear, newMonth);
-                return events;
-            }
-        };
-        // Get a reference for the week view in the layout.
-        mWeekView = findViewById(R.id.weekView);
+public class CalendarActivity extends AppCompatActivity {
 
-// Set an action when any event is clicked.
-        mWeekView.setOnEventClickListener(this);
+    private static final String TAG = "CalendarActivity"; // use TAG for Logging
 
-// The week view has infinite scrolling horizontally. We have to provide the events of a
-// month every time the month changes on the week view.
-        mWeekView.setMonthChangeListener(mMonthChangeListener);
-
-// Set long press listener for events.
-        mWeekView.setEventLongPressListener(this);
-
-    }
-
-    @Override
-    public List<? extends WeekViewEvent> onMonthChange(int newYear, int newMonth) {
-        return null;
-    }
-
-    @Override
-    public void onEventClick(WeekViewEvent event, RectF eventRect) {
-
-    }
-
-    @Override
-    public void onEventLongPress(WeekViewEvent event, RectF eventRect) {
-
-    }
-
-    private List<WeekViewEvent> getEvents(int newYear, int newMonth) {
-        List<WeekViewEvent> eventList = null;
-        WeekViewEvent newEvent = new WeekViewEvent();
-        Calendar calendar = Calendar.getInstance();
-        newEvent.setStartTime(calendar);
-        calendar.set(Calendar.HOUR, (calendar.HOUR + 1));
-        newEvent.setEndTime(calendar);
-        eventList.add(newEvent);
-        return eventList;
-    }
 }
-
-
