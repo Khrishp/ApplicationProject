876de78cabb63be22fb900065ee85596f6f73d6e
czzzz97 <cshughes97@yahoo.com>
2019-03-29 10:58:37 -0600

    Add recyclerView detection of user tapping

--- app/src/main/java/com/example/applicationproject/CalendarActivity.java
+++ app/src/main/java/com/example/applicationproject/CalendarActivity.java
@@ -3,20 +3,24 @@ package com.example.applicationproject;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.CalendarView;
+
 
 /**
  * Users will tap on the date they would like to view. This will take them to the
  * SignUp activity where they can manage dates/times of their work.
  * Potential ideas:
  * - Days with no more hours to fill will show in a different color like gray.
- * - Days that the particular user has at least one scheduled hour on will show up in a color (yellow?)
+ * - Days that the particular user has at least one scheduled hour on will show up in a color (red?)
  * - At the bottom, below the calendar, the user's next scheduled hour of work will appear. "Thursday, March 30 @ 3pm"
  */
 public class CalendarActivity extends AppCompatActivity{
+    private static final String TAG = "CalendarActivity"; // use TAG for Logging
     private CalendarView mCalendarView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.v(TAG, "Entered onCreate.");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
 
@@ -25,8 +29,9 @@ public class CalendarActivity extends AppCompatActivity{
         mCalendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                Log.v(TAG, "Setting dateChangeListener.");
                 String date = (month + 1) + "/" + dayOfMonth + "/" + year;
-
+                Log.v(TAG, "The date is: " + date);
                 Intent intent = new Intent(CalendarActivity.this, SignUpActivity.class);
                 intent.putExtra("date", date);
                 startActivity(intent);
@@ -34,5 +39,5 @@ public class CalendarActivity extends AppCompatActivity{
         });
 
     }
-
+    
 }
--- app/src/main/java/com/example/applicationproject/SignUpActivity.java
+++ app/src/main/java/com/example/applicationproject/SignUpActivity.java
@@ -3,9 +3,13 @@ package com.example.applicationproject;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * Here the User will see all of the hours they are signed up for on that date (with a tag so they know they signed up),
@@ -14,26 +18,60 @@ import android.widget.TextView;
  * An User with read-all permissions (ie Admin) would see EVERYONE's shifts for the day.
  */
 public class SignUpActivity extends AppCompatActivity {
+    private static final String TAG = "SignUpActivity";
     private TextView theDate;
     private Button calendarButton;
+    private ArrayList<String> openHours;
+    private RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.v(TAG, "Entering onCreate().");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
         theDate = findViewById(R.id.scheduleDate);
         calendarButton = findViewById(R.id.calendarButton);
+        mRecyclerView = findViewById(R.id.recycler);
 
+        //I have suspicions about this line. Is this right? It's from the tutorial I watched but...
         Intent incomingIntent = getIntent();
+
         String date = incomingIntent.getStringExtra("date");
         theDate.setText(date);
+        openHours = new ArrayList();
+        mRecyclerView.addOnItemTouchListener(new MyTouchListener(this,
+                mRecyclerView,
+                new MyTouchListener.OnTouchActionListener() {
+                    @Override
+                    public void onLeftSwipe(View view, int position) {
+//we don't need it at least for now
+                        Log.v(TAG, "Left swipe on recycler");
+                    }
+
+                    @Override
+                    public void onRightSwipe(View view, int position) {
+//might not need, but nice to have just in case
+                        Log.v(TAG, "Right swipe on recycler");
+                    }
+
+                    @Override
+                    public void onClick(View view, int position) {
+                        Log.v(TAG, "Clicked on recyclerView at position: " + position);
+                        //Check if signed up for already or not
+
+                        //If signed up already, remove that hour from the user's schedule
+
+                        //Else add that hour to the user's schedule
+
+                    }
+                }));
 
         calendarButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
+                Log.v(TAG, "CalendarButton.onClick listener");
                 Intent intent = new Intent(SignUpActivity.this, CalendarActivity.class);
                 startActivity(intent);
-
                 }
             }
         );
