a90815bcdd06316c6ae3d5a63f5e7529cf8dd0c1
czzzz97 <cshughes97@yahoo.com>
2019-03-30 17:26:26 -0600

    Create WriteNewsActivity

--- app/src/main/java/com/example/applicationproject/News.java
+++ app/src/main/java/com/example/applicationproject/News.java
@@ -1,10 +1,26 @@
 package com.example.applicationproject;
 
+import java.util.Date;
+
 // more of just a struct for news to be stored
 public class News {
     private String header;
-    private String subject;
-    private Integer date;
+    private String body;
+    private String date;
+
+    /**
+     * (H)eader will be the title above the news.
+     * (B)ody is the text of the news itself.
+     * And (D)ate will be automatic (no input from user): the date it was written.
+     * @param h
+     * @param b
+     * @param d
+     */
+    News(String h, String b, String d){
+        header = h;
+        body = b;
+        date = d;
+    }
 
     public String getHeader() {
         return header;
@@ -14,19 +30,19 @@ public class News {
         this.header = header;
     }
 
-    public String getSubject() {
-        return subject;
+    public String getBody() {
+        return body;
     }
 
-    public void setSubject(String subject) {
-        this.subject = subject;
+    public void setBody(String body) {
+        this.body = body;
     }
 
-    public Integer getDate() {
+    public String getDate() {
         return date;
     }
 
-    public void setDate(Integer date) {
+    public void setDate(String date) {
         this.date = date;
     }
 }
---
+++ app/src/main/java/com/example/applicationproject/WriteNewsActivity.java
@@ -0,0 +1,57 @@
+package com.example.applicationproject;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.type.Date;
+
+public class WriteNewsActivity extends AppCompatActivity {
+    private static final String TAG = "WriteNewsActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.v(TAG, "Creating...");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news);
+        Log.v(TAG, "Initializing/declaring variables...");
+        final FirebaseFirestore db = FirebaseFirestore.getInstance();
+        Date dateTest = Date.getDefaultInstance();
+        String now = (Integer.toString(dateTest.getMonth() + 1));
+        now += ("/" + Integer.toString(dateTest.getDay()));
+        now += ("/" + Integer.toString(dateTest.getYear()));
+        Log.v(TAG, "Today's date is: " + now);
+
+        TextView dateText = findViewById(R.id.dateText);
+        dateText.setText(now);
+        final Button makeNewsButton = findViewById(R.id.makeNewsButton);
+
+        makeNewsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText headerText = findViewById(R.id.headerText);
+                EditText bodyText = findViewById(R.id.bodyText);
+                Date today = Date.getDefaultInstance();
+                String date = (Integer.toString(today.getMonth() + 1));
+                date += ("/" + Integer.toString(today.getDay()));
+                date += ("/" + Integer.toString(today.getYear()));
+                final String header = headerText.getText().toString();
+                final String body = bodyText.getText().toString();
+                final News news = new News(header, body, date);
+                Toast.makeText(WriteNewsActivity.this, "News Created", Toast.LENGTH_LONG).show();
+                DocumentReference reference = db.collection("news").document(date + "-" + header);
+                reference.set(news);
+            }
+        });
+
+    }
+}
