b2ae4a06ba1e15f555ae33575dbf3ef3e19eed1a
czzzz97 <cshughes97@yahoo.com>
2019-04-06 14:49:21 -0600

    News now stores doc with id (sortable) rather than by date

--- app/src/main/java/com/example/applicationproject/Activities/WriteNewsActivity.java
+++ app/src/main/java/com/example/applicationproject/Activities/WriteNewsActivity.java
@@ -1,6 +1,7 @@
 package com.example.applicationproject.Activities;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -8,10 +9,16 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.example.applicationproject.Objects.News;
 import com.example.applicationproject.R;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
 
 
 /**
@@ -41,13 +48,27 @@ public class WriteNewsActivity extends AppCompatActivity {
                 final String header = headerText.getText().toString();
                 final String body = bodyText.getText().toString();
                 final String date = dateText.getText().toString();
-                final News news = new News(header, body, date);
-                Toast.makeText(WriteNewsActivity.this, "News Created", Toast.LENGTH_LONG).show();
-                DocumentReference reference = db.collection("news").document(date);
-                reference.set(news);
-                finish();
+                //get index for the news
+                db.collection("news")
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    int index = 1000;
+                                for (QueryDocumentSnapshot document : task.getResult()) {
+                                     index--;
+                                }
+                                    final News news = new News(header, body, date);
+                                    Toast.makeText(WriteNewsActivity.this, "News Created", Toast.LENGTH_LONG).show();
+                                    DocumentReference reference = db.collection("news").document(Integer.toString(index));
+                                    reference.set(news);
+                                    finish();
+                            }
             }
         });
 
+    }
+});
     }
 }
