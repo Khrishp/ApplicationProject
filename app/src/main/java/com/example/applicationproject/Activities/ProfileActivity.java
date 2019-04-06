eb177b61d1eafd74ec79374dd8211d147e2afe77
czzzz97 <cshughes97@yahoo.com>
2019-04-06 15:20:12 -0600

    Profile Activity hides the "Change job" button from non-admins.

--- app/src/main/java/com/example/applicationproject/Activities/ProfileActivity.java
+++ app/src/main/java/com/example/applicationproject/Activities/ProfileActivity.java
@@ -4,6 +4,8 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,6 +24,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
  */
 public class ProfileActivity extends AppCompatActivity {
 
+    User currentUser;
     User user;
     FirebaseAuth mAuth;
     FirebaseFirestore db;
@@ -39,12 +42,33 @@ public class ProfileActivity extends AppCompatActivity {
         final TextView phone = findViewById(R.id.phoneText);
         final TextView hours = findViewById(R.id.hoursText);
         final TextView job = findViewById(R.id.jobText);
+        final Button changeJobButton = findViewById(R.id.changeJobButton);
+
+        db = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+        
+        //won't need to change this chunk if working correctly. checks current user's permissions
+        changeJobButton.setVisibility(View.INVISIBLE);
+        DocumentReference doc = db.collection("users").document(mAuth.getCurrentUser().getEmail());
+        doc.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if(task.isSuccessful()){
+                    Log.d(TAG, "task successful");
+                    DocumentSnapshot documentAcc = task.getResult();
+                    if (documentAcc.exists()) {
+                        currentUser = documentAcc.toObject(User.class);
+                        if(currentUser.canEditInterns()) {
+                            changeJobButton.setVisibility(View.VISIBLE);
+                        }
+                    }
+                }
+            }
+        });
 
 
         Log.d(TAG, "declared textviews");
 
-        db = FirebaseFirestore.getInstance();
-        mAuth = FirebaseAuth.getInstance();
 
         DocumentReference docRef = db.collection("users").document(mAuth.getCurrentUser().getEmail());
         docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
